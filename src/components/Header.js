a9885bf3c8825f3d5b8d138998bf433088835c19
cmarik <colton.marik99@gmail.com>
2020-11-25 16:58:29 -0500

    Web2

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <img src="logo.png"  />
+      <img src={logo.png} alt="" />
     </div>
     <div className="content">
       <div className="inner">
