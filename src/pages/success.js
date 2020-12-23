43ba77542ee60c1fbd4f2a29dac4e85b380ab64e
cmarik <colton.marik99@gmail.com>
2020-12-23 16:55:28 -0500

    Adding Success Page

---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Success = () => (
+  <Layout>
+    <h1>Success!</h1>
+    <p>You successfully submitted a form.</p>
+  </Layout>
+)
+
+export default Success
